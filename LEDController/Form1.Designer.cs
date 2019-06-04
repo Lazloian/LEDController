f9a632361a09046d532dc78e1a414c2fe8ce4e5f
Henry Silva <henry.silva.999@gmail.com>
2019-06-04 16:59:38 -0700

    Add files via upload

---
+++ LEDController/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace LEDController
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(102, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "On";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(102, 51);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Off";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.LargeChange = 25;
+            this.trackBar1.Location = new System.Drawing.Point(23, 140);
+            this.trackBar1.Maximum = 255;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar1.Size = new System.Drawing.Size(45, 218);
+            this.trackBar1.SmallChange = 25;
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.LargeChange = 25;
+            this.trackBar2.Location = new System.Drawing.Point(102, 140);
+            this.trackBar2.Maximum = 255;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar2.Size = new System.Drawing.Size(45, 218);
+            this.trackBar2.SmallChange = 25;
+            this.trackBar2.TabIndex = 3;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.LargeChange = 25;
+            this.trackBar3.Location = new System.Drawing.Point(188, 140);
+            this.trackBar3.Maximum = 255;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar3.Size = new System.Drawing.Size(45, 218);
+            this.trackBar3.SmallChange = 25;
+            this.trackBar3.TabIndex = 4;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(23, 389);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(41, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(102, 389);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(41, 20);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(188, 389);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(41, 20);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(84, 415);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Send";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(105, 491);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Save";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(23, 464);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(71, 21);
+            this.comboBox1.TabIndex = 10;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(105, 461);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "Load";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(188, 51);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 12;
+            this.button6.Text = "Random";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(23, 491);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(73, 20);
+            this.textBox4.TabIndex = 13;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(186, 461);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 14;
+            this.button7.Text = "Delete";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(294, 539);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ LEDController/Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.IO.Ports;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace LEDController
+{
+    public partial class Form1 : Form
+    {
+        SerialPort sp = new SerialPort("COM3", 9600, Parity.None, 8, StopBits.One);
+        IDictionary<string, int> r = new Dictionary<string, int>();
+        IDictionary<string, int> g = new Dictionary<string, int>();
+        IDictionary<string, int> b = new Dictionary<string, int>();
+        Random random = new Random();
+
+        int random_button = 0;
+        int r_wave = 85;
+        int g_wave = 170;
+        int b_wave = 255;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sp.Open();
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\Public\Documents\Saves.txt");
+            int g_place = 0,
+                b_place = 0,
+                space_place = 0,
+                red = 0,
+                green = 0,
+                blue = 0,
+                green_stop = 0,
+                blue_stop = 0;
+            string color = "";
+
+
+            foreach (string line in lines)
+            {
+                if (line == "") //skips the new line character
+                {
+
+                }
+                else if (line.Substring(0, 1) == "r")
+                {
+                    System.Diagnostics.Debug.WriteLine(line.Length);
+                    System.Diagnostics.Debug.WriteLine(line.Substring(1, 1));
+                    for (int i = 0; i < line.Length; i++)
+                    {
+                        if ((line.Substring(i, 1) == "g") && (green_stop == 0))
+                        {
+                            g_place = i;
+                            red = Int32.Parse(line.Substring(1, g_place - 1));
+                            green_stop = 1;
+                        }
+                        else if ((line.Substring(i, 1) == "b") && (blue_stop == 0))
+                        {
+                            b_place = i;
+                            green = Int32.Parse(line.Substring(g_place + 1, b_place - g_place - 1));
+                            blue_stop = 1;
+                        }
+                        else if (line.Substring(i, 1) == " ")
+                        {
+                            space_place = i;
+                            blue = Int32.Parse(line.Substring(b_place + 1, space_place - b_place - 1));
+                            color = line.Substring(space_place + 1, line.Length - space_place - 1);
+                        }
+                    }
+                    r.Add(color, red);
+                    g.Add(color, green);
+                    b.Add(color, blue);
+                    comboBox1.Items.Add(color);
+                    System.Diagnostics.Debug.WriteLine("Color " + color + " Added (" + red + " " + green + " " + blue + ")");
+                    green_stop = 0;
+                    blue_stop = 0;
+                }
+            }
+            System.Diagnostics.Debug.WriteLine("Form Loaded");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sp.Write("1");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            sp.Write("0");
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            textBox1.Text = "" + trackBar1.Value;
+            System.Diagnostics.Debug.WriteLine("r" + trackBar1.Value);
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            textBox2.Text = "" + trackBar2.Value;
+            System.Diagnostics.Debug.WriteLine("g" + trackBar2.Value);
+        }
+
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+            textBox3.Text = "" + trackBar3.Value;
+            System.Diagnostics.Debug.WriteLine("b" + trackBar3.Value);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            trackBar1.Value = Int32.Parse(textBox1.Text);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            trackBar2.Value = Int32.Parse(textBox2.Text);
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            trackBar3.Value = Int32.Parse(textBox3.Text);
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            sp.Write("r" + trackBar1.Value + "g" + trackBar2.Value + "b" + trackBar3.Value);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            r.Add(textBox4.Text, trackBar1.Value);
+            g.Add(textBox4.Text, trackBar2.Value);
+            b.Add(textBox4.Text, trackBar3.Value);
+            comboBox1.Items.Add(textBox4.Text);
+            System.IO.File.AppendAllText(@"C:\Users\Public\Documents\Saves.txt", "r" + trackBar1.Value + "g" + trackBar2.Value + "b" + trackBar3.Value + " " + textBox4.Text + "\r\n");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            sp.Write("r" + r[comboBox1.Text] + "g" + g[comboBox1.Text] + "b" + b[comboBox1.Text]);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            System.Timers.Timer rTimer = new System.Timers.Timer(2000);
+            rTimer.AutoReset = true;
+
+            rTimer.Enabled = true;
+            rTimer.AutoReset = true;
+            rTimer.Elapsed += RandomColors;
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\Public\Documents\Saves.txt");
+
+            string color = "";
+
+            foreach (string line in lines) {
+                if (line == "") //skips new line character
+                {
+                }
+                else if (line.Substring(0 ,1) == "r") {
+                    for (int i = 0; i < line.Length; i++) {
+                        if (line.Substring(i, 1) == " ") {
+                            color = line.Substring(i + 1, line.Length - i - 1);
+                            System.Diagnostics.Debug.WriteLine(color);
+                            if (color != comboBox1.Text) {
+                                System.IO.File.AppendAllText(@"C:\Users\Public\Documents\Overwrite.txt", line + "\r\n");
+
+                            }
+                        }
+                    }
+                }
+            }
+            r.Remove(comboBox1.Text);
+            g.Remove(comboBox1.Text);
+            b.Remove(comboBox1.Text);
+            comboBox1.Items.Remove(comboBox1.Text);
+
+            System.IO.File.WriteAllText(@"C:\Users\Public\Documents\Saves.txt", "");
+            string[] newLines = System.IO.File.ReadAllLines(@"C:\Users\Public\Documents\Overwrite.txt");
+
+            foreach (string line in newLines)
+            {
+                System.IO.File.AppendAllText(@"C:\Users\Public\Documents\Saves.txt", line + "\r\n");
+            }
+            //Clear the overwrite file after deletion is complete
+            System.IO.File.WriteAllText(@"C:\Users\Public\Documents\Overwrite.txt", "");
+        }
+
+        private void RandomColors(Object source, ElapsedEventArgs e)
+        {
+            int r = random.Next(0, 256);
+            int g = random.Next(0, 256);
+            int b = random.Next(0, 256);
+            sp.Write("r" + r + "g" + g + "b" + b);
+        }
+
+        private void ColorWave(Object source, ElapsedEventArgs e)
+        {
+            r_wave += 17;
+            g_wave += 17;
+            b_wave += 17;
+
+            if (r_wave > 255)
+            {
+                r_wave = r_wave - 255;
+            }
+            if (g_wave > 255)
+            {
+                g_wave = g_wave - 255;
+            }
+            if (b_wave > 255)
+            {
+                b_wave = b_wave - 255;
+            }
+
+            sp.Write("r" + r_wave + "g" + g_wave + "b" + b_wave);
+        }
+    }
+}
